167410b932c7ff19c47cc48b87afaa45a536cd9d
ikorin24 <sunshinexxab@yahoo.co.jp>
2023-03-02 21:08:59 +0900

    update

--- Assets/IwasmUnity/Scripts/IwasmCApi.cs
+++ Assets/IwasmUnity/Scripts/IwasmCApi.cs
@@ -66,6 +66,14 @@ namespace IwasmUnity
         private static extern void wasm_byte_vec_delete(wasm_byte_vec_t* bytes);
 
         [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
+        private static extern void wasm_valtype_vec_new(wasm_valtype_vec_t* vec, size_t size, wasm_valtype_t** data);
+
+        [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
+        private static extern void wasm_valtype_vec_new_uninitialized(
+            wasm_valtype_vec_t* out_vec,
+            size_t size);
+
+        [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
         private static extern void wasm_extern_vec_delete(wasm_extern_vec_t* externs);
 
         [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
@@ -75,7 +83,64 @@ namespace IwasmUnity
             wasm_val_vec_t* results);
 
         [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
+        private static extern wasm_functype_t_ptr wasm_functype_new(
+            wasm_valtype_vec_t* parameters,
+            wasm_valtype_vec_t* results);
+
+        [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
+        private static extern wasm_func_t_ptr wasm_func_new(
+            wasm_store_t_ptr store,
+            wasm_functype_t_ptr functype,
+            IntPtr callback     // delegate* unmanaged[Cdecl]<wasm_val_vec_t*, wasm_val_vec_t*, wasm_trap_t_ptr>
+            );
+
+        [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
+        private static extern void wasm_functype_delete(wasm_functype_t_ptr func_type);
+
+
+        [DllImport(Iwasm.DllName, CallingConvention = Cdecl)]
         private static extern void wasm_trap_delete(wasm_trap_t_ptr trap);
+
+
+        static wasm_functype_t_ptr wasm_functype_new_0_0()
+        {
+            wasm_valtype_vec_t parameters;
+            wasm_valtype_vec_t results;
+            wasm_valtype_vec_new_uninitialized(&parameters, new size_t(0));
+            wasm_valtype_vec_new_uninitialized(&results, new size_t(0));
+            return wasm_functype_new(&parameters, &results);
+        }
+
+        static wasm_functype_t_ptr wasm_functype_new_N_0(wasm_valtype_t** p, int paramCount)
+        {
+            wasm_valtype_vec_t parameters;
+            wasm_valtype_vec_t results;
+            wasm_valtype_vec_new(&parameters, new size_t((uint)paramCount), p);
+            wasm_valtype_vec_new_uninitialized(&results, new size_t(0));
+            return wasm_functype_new(&parameters, &results);
+        }
+
+        static void hoge()
+        {
+            var parameters = stackalloc wasm_valtype_t*[2]
+            {
+                default,
+                default,
+            };
+            var f = wasm_functype_new_N_0(parameters, 2);
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate wasm_trap_t_ptr ImportDelegate(wasm_val_vec_t* args, wasm_val_vec_t* results);
+
+        private static ImportDelegate _helloCallback = HelloCallback;
+
+        [AOT.MonoPInvokeCallback(typeof(ImportDelegate))]
+        private static wasm_trap_t_ptr HelloCallback(wasm_val_vec_t* args, wasm_val_vec_t* results)
+        {
+            UnityEngine.Debug.Log("hello !!");
+            return wasm_trap_t_ptr.Null;
+        }
 
         private static void Sample(byte[] wasm)
         {
@@ -116,25 +181,32 @@ namespace IwasmUnity
                     }
                     try
                     {
-                        // TODO: not implemented yet
-                        //own wasm_functype_t*hello_type = wasm_functype_new_0_0();
-                        //own wasm_func_t*hello_func =
-                        //  wasm_func_new(store, hello_type, hello_callback);
 
-                        //wasm_functype_delete(hello_type);
+                        wasm_functype_t_ptr hello_type = wasm_functype_new_0_0();
+                        wasm_func_t_ptr hello_func;
+                        try
+                        {
+                            hello_func = wasm_func_new(store, hello_type, Marshal.GetFunctionPointerForDelegate(_helloCallback));
+                        }
+                        finally
+                        {
+                            wasm_functype_delete(hello_type);
+                        }
 
-                        ////wasm_extern_t* externs[] = { wasm_func_as_extern(hello_func) };
-                        //wasm_extern_t_ptr externs = { wasm_func_as_extern(hello_func) };
+                        const int ExternsCount = 1;
+                        var externs = stackalloc wasm_extern_t_ptr[ExternsCount]
+                        {
+                            wasm_func_as_extern(hello_func),
+                        };
 
+                        wasm_extern_vec_t imports = new wasm_extern_vec_t(
+                            new size_t((uint)sizeof(wasm_extern_t_ptr) * ExternsCount),
+                            externs,
+                            new size_t((uint)ExternsCount),
+                            new size_t((uint)sizeof(wasm_extern_t_ptr)),
+                            null);
 
-                        //wasm_extern_vec_t imports = WASM_ARRAY_VEC(externs);
-                        //own wasm_instance_t*instance =
-                        //  wasm_instance_new(store, module, &imports, NULL);
-
-                        //var instance = wasm_instance_new(store, module, &imports, null);
-
-
-                        var instance = wasm_instance_new(store, module, null, null);
+                        var instance = wasm_instance_new(store, module, &imports, null);
                         try
                         {
                             wasm_extern_vec_t exports;
@@ -215,8 +287,20 @@ namespace IwasmUnity
     internal readonly struct wasm_trap_t_ptr
     {
         private readonly IntPtr _p;
+
+        public static wasm_trap_t_ptr Null => default;
         public bool IsNull => _p == IntPtr.Zero;
     }
+
+    internal readonly struct wasm_functype_t_ptr
+    {
+        private readonly IntPtr _p;
+    }
+
+    internal readonly struct wasm_valtype_t
+    {
+        public readonly wasm_valkind_t kind;
+    };
 
     internal unsafe readonly struct wasm_byte_vec_t
     {
@@ -231,6 +315,29 @@ namespace IwasmUnity
     {
         public readonly size_t size;
         public readonly wasm_extern_t_ptr* data;
+        public readonly size_t num_elems;
+        public readonly size_t size_of_elem;
+        public readonly void* _lock;
+
+        public wasm_extern_vec_t(
+            size_t size,
+            wasm_extern_t_ptr* data,
+            size_t num_elems,
+            size_t size_of_elem,
+            void* @lock)
+        {
+            this.size = size;
+            this.data = data;
+            this.num_elems = num_elems;
+            this.size_of_elem = size_of_elem;
+            this._lock = @lock;
+        }
+    }
+
+    internal unsafe readonly struct wasm_valtype_vec_t
+    {
+        public readonly size_t size;
+        public readonly wasm_valtype_t** data;
         public readonly size_t num_elems;
         public readonly size_t size_of_elem;
         public readonly void* _lock;
